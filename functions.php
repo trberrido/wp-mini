8b6ca3e52abe9f2759569c343f559ac3d4d71100
trberrido <thibault.berrido@gmail.com>
2024-02-09 16:38:20 +0100

    set correct function prefox

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php defined('ABSPATH') or die();
 
 // Load all the required php files
-add_action( 'after_setup_theme', 'dw__get_inc_files' );
-function dw__get_inc_files() {
+add_action( 'after_setup_theme', 'wpmini__get_inc_files' );
+function wpmini__get_inc_files() {
 	foreach ( glob( get_stylesheet_directory() . '/inc/*.php' ) as $file ) {
 		include_once $file;
 	}
